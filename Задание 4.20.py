98737ed5674b99213500d47abe539fd1006040a0
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2024-01-12 16:42:47 +0500

    Поиск столбцов с адресами
    1. Настроен поиск адресов
    2. Убраны в комментарии блоки по поиску ссылок и пола
    3. Изменен размер окна вывода результатов

--- Задание 4.20.py
+++ Задание 4.20.py
@@ -12,7 +12,7 @@ import re
 #DESIGN
 # Создание главного окна
 window = tk.Tk()
-window.geometry("900x690")
+window.geometry("600x550")
 window.title("Программа анализа .csv файлов")
 
 # Создание меток вывода
@@ -118,35 +118,35 @@ label_180.place(x=40, y=495)
 label_181 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
 label_181.place(x=220, y=495)
 
-label_191 = tk.Label(window, text = "Ссылка", font = ('Helvetica', 10, 'bold'), fg = '#008800')
-label_191.place(x=220, y=530)
+# ~ label_191 = tk.Label(window, text = "Ссылка", font = ('Helvetica', 10, 'bold'), fg = '#008800')
+# ~ label_191.place(x=220, y=530)
 
-label_200 = tk.Label(window, text = "Номер столбца", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_200.place(x=40, y=550)
+# ~ label_200 = tk.Label(window, text = "Номер столбца", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_200.place(x=40, y=550)
 
-label_201 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_201.place(x=220, y=550)
+# ~ label_201 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_201.place(x=220, y=550)
 
-label_210 = tk.Label(window, text = "Количество значений", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_210.place(x=40, y=575)
+# ~ label_210 = tk.Label(window, text = "Количество значений", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_210.place(x=40, y=575)
 
-label_211 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_211.place(x=220, y=575)
+# ~ label_211 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_211.place(x=220, y=575)
 
-label_221 = tk.Label(window, text = "Пол", font = ('Helvetica', 10, 'bold'), fg = '#008800')
-label_221.place(x=220, y=610)
+# ~ label_221 = tk.Label(window, text = "Пол", font = ('Helvetica', 10, 'bold'), fg = '#008800')
+# ~ label_221.place(x=220, y=610)
 
-label_230 = tk.Label(window, text = "Номер столбца", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_230.place(x=40, y=630)
+# ~ label_230 = tk.Label(window, text = "Номер столбца", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_230.place(x=40, y=630)
 
-label_231 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_231.place(x=220, y=630)
+# ~ label_231 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_231.place(x=220, y=630)
 
-label_240 = tk.Label(window, text = "Количество значений", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_240.place(x=40, y=655)
+# ~ label_240 = tk.Label(window, text = "Количество значений", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_240.place(x=40, y=655)
 
-label_241 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
-label_241.place(x=220, y=655)
+# ~ label_241 = tk.Label(window, text = "", font = ('Helvetica', 10, 'bold'), fg = '#000000')
+# ~ label_241.place(x=220, y=655)
 
 
 #COMMON
@@ -236,11 +236,11 @@ def count_tel_number(df):
         if item <= 1: 
             b = 0
         else:
-            lst = get_list_column(df, index)   # получи столбец из таблицы
-            mySeries = pd.Series(lst)          # сделай из этого столбца массив pandas Series индекс / значение
-            data = mySeries.value_counts()     # сверни массив данных по уникальным значениям и посчитай их частотность
+            lst = get_list_column(df, index)   # получаю столбец из таблицы
+            mySeries = pd.Series(lst)          # делаю из этого столбца массив pandas Series индекс / значение
+            data = mySeries.value_counts()     # сверачиваю массив данных по уникальным значениям и считаю их частотность
             # ~ print(data)
-            b = sum(data)                      # суммируй количество данных. Если нужно вывести количество уникальных значений, можно написать len(data)
+            b = sum(data)                      # суммирую количество данных. Если нужно вывести количество уникальных значений, можно написать len(data)
         arr1.append(b)
     return arr1
 
@@ -297,13 +297,13 @@ def count_firstname(df):
         if item <= 0: 
             b = 0
         else:
-            lst = get_list_column(df, index)   # получи столбец из таблицы
-            mySeries = pd.Series(lst)          # сделай из этого столбца массив pandas Series индекс / значение
-            # ~ print(mySeries[421])           # проверка по файлу Contact.csv типа данных в ячейке со значением NaN
+            lst = get_list_column(df, index)   # получаю столбец из таблицы
+            mySeries = pd.Series(lst)          # селаю из этого столбца массив pandas Series индекс / значение
+            # ~ print(mySeries[421])           # проверяю по файлу Contact.csv типа данных в ячейке со значением NaN
             # ~ print(type(mySeries[421]))
-            data = mySeries.value_counts()     # сверни массив данных по уникальным значениям и посчитай их частотность
+            data = mySeries.value_counts()     # сверачиваю массив данных по уникальным значениям и считаю их частотность
             # ~ print(data)
-            b = sum(data)                      # суммируй количество данных. Если нужно вывести количество уникальных значений, можно написать len(data)
+            b = sum(data)                      # суммирую количество данных. Если нужно вывести количество уникальных значений, можно написать len(data)
         arfn1.append(b)
     return arfn1
 
@@ -351,13 +351,43 @@ def find_lastname_column(df):
         arln.append(a)
     return arln
 
+# Подсчет количества фамилий в найденных столбцах
+def count_lastname(df):
+    numbers = find_lastname_column(df)
+    arln1 = []
+    b=0
+    for index, item in enumerate(numbers):
+        if item <= 0: 
+            b = 0
+        else:
+            lst = get_list_column(df, index)
+            mySeries = pd.Series(lst)
+            data = mySeries.value_counts()
+            # ~ print(data)
+            b = sum(data)
+        arln1.append(b)
+    return arln1
 
+# Определение результата по lastname
+def get_result_lastname(df):
+    arln1 = count_lastname(df)
+    arln2 = []
+    arln3 = []
+    for index, item in enumerate(arln1):
+        if item <= 0:
+            continue
+        else:
+            a = index+1
+            arln2.append(a)
+            b = item
+            arln3.append(b)
+    return arln2, arln3
 
-file_name = do_dialog()
-df = pandas_read_csv(file_name)
-arln = find_lastname_column(df)
-print(arln)
-print(type(arln))
+# ~ file_name = do_dialog()
+# ~ df = pandas_read_csv(file_name)
+# ~ arln = find_lastname_column(df)
+# ~ print(arln)
+# ~ print(type(arln))
 # ~ arln1 = count_lastname(df)
 # ~ print(arln1)
 # ~ result = get_result_lastname(df)
@@ -377,7 +407,7 @@ def find_secondname_column(df):
         for item in lst:
             if isinstance(item, float) and np.isnan(item):
                 continue
-            elif re.findall(r'Александр\b|Анна\b|Михаил\b|Ольга\b', item):
+            elif re.findall(r'\Bвич|\Bвна', item):
                 a += 1
         arsn.append(a)
     return arsn
@@ -391,13 +421,11 @@ def count_secondname(df):
         if item <= 0: 
             b = 0
         else:
-            lst = get_list_column(df, index)   # получи столбец из таблицы
-            mySeries = pd.Series(lst)          # сделай из этого столбца массив pandas Series индекс / значение
-            # ~ print(mySeries[421])           # проверка по файлу Contact.csv типа данных в ячейке со значением NaN
-            # ~ print(type(mySeries[421]))
-            data = mySeries.value_counts()     # сверни массив данных по уникальным значениям и посчитай их частотность
+            lst = get_list_column(df, index)
+            mySeries = pd.Series(lst) 
+            data = mySeries.value_counts()
             # ~ print(data)
-            b = sum(data)                      # суммируй количество данных. Если нужно вывести количество уникальных значений, можно написать len(data)
+            b = sum(data)
         arsn1.append(b)
     return arsn1
 
@@ -421,11 +449,74 @@ def get_result_secondname(df):
 # ~ arsn = find_secondname_column(df)
 # ~ print(arsn)
 # ~ print(type(arsn))
-# ~ arsn1 = count_firstname(df)
+# ~ arsn1 = count_secondname(df)
 # ~ print(arsn1)
 # ~ result = get_result_secondname(df)
 # ~ print(result)
 # ~ print(type(result))
+
+
+
+#ADDRESS
+# Поиск столбцов с адресами по заданным шаблонам
+def find_address_column(df):
+    cnt_columns = df.shape[1]
+    arad = []
+    for i in range(cnt_columns):
+        lst = get_list_column(df, i)
+        a = 0
+        for item in lst:
+            if isinstance(item, float) and np.isnan(item):
+                continue
+            elif re.findall(r'ул\.|str\.', item):
+                a += 1
+        arad.append(a)
+    return arad
+
+# Подсчет количества адресов в найденных столбцах
+def count_address(df):
+    numbers = find_address_column(df)
+    arad1 = []
+    b=0
+    for index, item in enumerate(numbers):
+        if item <= 0: 
+            b = 0
+        else:
+            lst = get_list_column(df, index)
+            mySeries = pd.Series(lst)
+            data = mySeries.value_counts()
+            # ~ print(data)
+            b = sum(data)
+        arad1.append(b)
+    return arad1
+
+# Определение результата по address
+def get_result_address(df):
+    arad1 = count_address(df)
+    arad2 = []
+    arad3 = []
+    for index, item in enumerate(arad1):
+        if item <= 0:
+            continue
+        else:
+            a = index+1
+            arad2.append(a)
+            b = item
+            arad3.append(b)
+    return arad2, arad3
+
+# ~ file_name = do_dialog()
+# ~ df = pandas_read_csv(file_name)
+# ~ arad = find_address_column(df)
+# ~ print(arad)
+# ~ print(type(arad))
+# ~ arad1 = count_address(df)
+# ~ print(arad1)
+# ~ result = get_result_address(df)
+# ~ print(result)
+# ~ print(type(result))
+
+
 
 
 #RESULTS
@@ -485,51 +576,49 @@ def fill_secondname_label(file_name):
         label_151['text'] = 'нет данных'
 
 # Вывод результата по address
-def get_result_address(file_name):
+def fill_address_label(file_name):
     df = pandas_read_csv(file_name)
-    try:
-        index, max_number = get_max_column(df)
-        label_171['text'] = index
-        label_181['text'] = max_number        
-    except:
+    arad2, arad3 = get_result_address(df)
+    if arad2 and arad3:
+        label_171['text'] = arad2
+        label_181['text'] = arad3        
+    else:
         label_171['text'] = 'нет данных'
         label_181['text'] = 'нет данных'
-    # ~ return df
 
-# Вывод результата по link
-def get_result_link(file_name):
-    df = pandas_read_csv(file_name)
-    try:
-        index, max_number = get_max_column(df)
-        label_201['text'] = index
-        label_211['text'] = max_number        
-    except:
-        label_201['text'] = 'нет данных'
-        label_211['text'] = 'нет данных'
-    # ~ return df
+# ~ # Вывод результата по link
+# ~ def get_result_link(file_name):
+    # ~ df = pandas_read_csv(file_name)
+    # ~ try:
+        # ~ index, max_number = get_max_column(df)
+        # ~ label_201['text'] = index
+        # ~ label_211['text'] = max_number        
+    # ~ except:
+        # ~ label_201['text'] = 'нет данных'
+        # ~ label_211['text'] = 'нет данных'
 
-# Вывод результата по gender
-def get_result_gender(file_name):
-    df = pandas_read_csv(file_name)
-    try:
-        index, max_number = get_max_column(df)
-        label_231['text'] = index
-        label_241['text'] = max_number        
-    except:
-        label_231['text'] = 'нет данных'
-        label_241['text'] = 'нет данных'
-    # ~ return df
+# ~ # Вывод результата по gender
+# ~ def get_result_gender(file_name):
+    # ~ df = pandas_read_csv(file_name)
+    # ~ try:
+        # ~ index, max_number = get_max_column(df)
+        # ~ label_231['text'] = index
+        # ~ label_241['text'] = max_number        
+    # ~ except:
+        # ~ label_231['text'] = 'нет данных'
+        # ~ label_241['text'] = 'нет данных'
+
 
 # Обработчик нажатия кнопки
 def process_button():
     file_name=do_dialog()
     label_01['text'] = file_name
-    # ~ fill_email_label(file_name)
-    # ~ fill_telephone_label(file_name)
-    # ~ fill_firstname_label(file_name)
-    # ~ fill_lastname_label(file_name)
-    # ~ fill_secondname_label(file_name)
-    # ~ get_result_address(file_name)
+    fill_email_label(file_name)
+    fill_telephone_label(file_name)
+    fill_firstname_label(file_name)
+    fill_lastname_label(file_name)
+    fill_secondname_label(file_name)
+    fill_address_label(file_name)
     # ~ get_result_link(file_name)
     # ~ get_result_gender(file_name)
     mb.showinfo(title=file_name, message = "Готово")
@@ -540,6 +629,3 @@ button.grid(row=4, column=1)
 
 # Запуск цикла mainloop
 window.mainloop()
-
-
-
